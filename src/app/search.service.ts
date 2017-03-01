c378f0f678b8a595657af29405f171f07323a5d5
Timothy Tavarez <timothy.tavarez@muse.codes>
2017-02-28 21:10:19 -0700

    Working angular-elastic service

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@angular/core';
+import * as elasticsearch from 'elasticsearch';
 
 @Injectable()
 export class SearchService {
 
   constructor() { }
+
+  createClient() {
+    let client = new elasticsearch.Client({
+      host: 'http://search-unifier-backend-7ukbssnzsrg4e54exrkr2j637q.us-west-2.es.amazonaws.com',
+      log: 'trace'
+    });
+
+    return client;
+  }
+
+  query(q) {
+    let client = this.createClient();
+
+    client.search({
+      q: q
+    }).then(body => {
+      console.log(body)
+    })
+  }
 
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,22 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { SearchService } from '../search.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
+  providers: [SearchService]
 })
 export class SearchComponent implements OnInit {
-query: string
-  constructor() {
+query: string;
+resultsText: string = '';
 
+  constructor(public search: SearchService) {
   }
 
   ngOnInit() {
   }
 
   onSubmit(f: NgForm) {
-    console.log(f.value);
+    console.log(f.value.query);
+
+    // this.search.query(f.value.query);
   }
 
 }
